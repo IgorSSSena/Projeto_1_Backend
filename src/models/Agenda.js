898499723439cca21a14900a88b14048e7e17106
Leonardo Kiyoshi <kiyoshi.leonardo@gmail.com>
2025-06-29 18:15:54 -0300

    Remove versão antiga

--- src/models/Agenda.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AgendaSchema = new mongoose.Schema({
-  idUsuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-  nome: String,
-  compromissos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Compromisso' }]
-});
-
-module.exports = mongoose.model('Agenda', AgendaSchema);
--- src/routes/notificacao.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const NotificacaoController = require('../controllers/NotificacaoController');
-
-router.get('/pendentes', NotificacaoController.listarPendentes);
-router.post('/enviar/:id', NotificacaoController.enviar);
-
-module.exports = router;
